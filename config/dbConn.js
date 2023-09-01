f77dcc46019600390c8678ef15edad4382fb8370
AustynYou <austyn.you@gmail.com>
2023-09-01 10:06:42 -0700

    Reflect edited and deleted slack message in real time

--- config/dbConn.js
+++ config/dbConn.js
@@ -3,8 +3,12 @@ const mongoose = require('mongoose')
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.DATABASE_URI)
-    } catch (err) {
-        console.log(err)
+    }  catch (error) {
+        if (error instanceof mongoose.Error) {
+            console.error('MongoDB Error:', error.message);
+        } else {
+            console.error('An error occurred while connecting to MongoDB:', error.message);
+        }
     }
 }
 
--- routes/slackEvents.js
+++ routes/slackEvents.js
@@ -5,16 +5,36 @@ const Message = require('../models/Message');
 const slackEvents = createEventAdapter(process.env.SLACK_SIGNING_SECRET);
 
 slackEvents.on('message', async (event, respond) => {
-  const newMessage = new Message({
-    slackId: event.client_msg_id,
-    text: event.text,
-    user: event.user,
-    channel: event.channel,
-    postedDate: new Date(parseInt(event.ts) * 1000).toISOString(),
-  });
+  console.log(event);
 
-  await newMessage.save();
-  console.log(`Message saved to MongoDB: ${newMessage.text}`);
+  if (event.subtype === 'message_changed') {
+    
+    const message = await Message.findOne({ slackId: event.previous_message.client_msg_id });
+
+    message.text = event.message.text;
+
+    const updatedMessage = await message.save();
+    console.log(`Message updated to MongoDB: ${updatedMessage.text}`);
+
+  } else if (event.subtype === 'message_deleted') {
+
+    await Message.findOneAndDelete({ slackId: event.previous_message.client_msg_id });
+
+    console.log(`Message deleted`);
+
+  } else {
+
+    const newMessage = new Message({
+      slackId: event.client_msg_id,
+      text: event.text,
+      user: event.user,
+      channel: event.channel,
+      postedDate: new Date(parseInt(event.ts) * 1000).toISOString(),
+    });
+  
+    await newMessage.save();
+    console.log(`Message saved to MongoDB: ${newMessage.text}`);
+  } 
 });
 
 slackEvents.on('error', (error, respond) => {
